b93a932fffbd211c1dbb3841636594168fbf09d9
Horoli <sunha321@gmail.com>
2023-10-04 12:14:42 +0900

    update

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,6 +1,17 @@
+
+const Path = require("path");
+const Fs = require("fs");
+
 module.exports = {
-  "POST /aaa": {
+  "GET /aaa": {
     // middlewares:[],
-    async handler(req, rep) {},
+    async handler(req, res) {
+      const path = Path.join(__dirname, '..', 'index.html');
+      const getHtml = Fs.readFileSync(path).toString('utf-8');
+
+      console.log(getHtml);
+
+      res.type("text/html").send(getHtml);
+    },
   },
 };
