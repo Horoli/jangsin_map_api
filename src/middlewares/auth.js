b8c839d7e0a02a6f50b5c9067755b39bcc82828a
Horoli <sunha321@gmail.com>
2023-10-27 20:04:38 +0900

    update

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,27 +8,23 @@ module.exports = async (req, rep) => {
   const usersCol = await MongoDB.getCollection("users");
 
   if (!token) {
-    return Utility.ERROR("token", "Not permitted(please input token)", 403);
+    return Utility.ERROR("token", "Not permitted(please input token)", 503);
   }
 
-
-
   const tokenInfo = await tokensCol.findOne({ token: token });
-  console.log('tokenInfo', tokenInfo)
-  if (tokenInfo === undefined || tokenInfo === null) {
-
+  if (tokenInfo === undefined) {
     return Utility.ERROR(
       "token",
       "Not permitted(tokenInfo is undefined.)",
-      403
+      503
     );
   } else if (Date.now() - tokenInfo.expireAt > 0) {
     await tokensCol.deleteOne({ token: token });
-    return Utility.ERROR("token", "token Expired", 403);
+    return Utility.ERROR("token", "token Expired", 503);
   }
 
-  tokenInfo.expireAt = Date.now() + Utility.TOKEN_EXPIRE_TIME;
-  // tokenInfo.expireAt = Date.now();
+  tokenInfo.expireAt = Date.now() + 1 * 30 * 6 * 1000;
+  // tokenInfo.expireAt = Date.now() + 1;
 
   req.token = tokenInfo;
   req.user = await usersCol.findOne({ id: tokenInfo.id });
